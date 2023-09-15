427f56d97dcee4701f17dde84352065b21e7bb7e
Noushadozi <noushadozi333@gmail.com>
2023-09-15 16:58:08 +0600

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='header-heading'>Udemy Registration</h1>
+            <img src="icons8-udemy-120.png" alt="" />
+        </div>
+    )
+}
+
+export default Header
